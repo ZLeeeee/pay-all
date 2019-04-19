869ea104c082b9a592171616cc713486642d1f93
zhaoli <501914148@qq.com>
2019-04-19 17:26:43 +0800

    增加支付方式权限查询，及联动修改

--- pay-dao/src/main/java/com/chaotu/pay/dao/TUserRatesMapper.java
+++ pay-dao/src/main/java/com/chaotu/pay/dao/TUserRatesMapper.java
@@ -3,6 +3,7 @@ package com.chaotu.pay.dao;
 import com.chaotu.pay.po.TUser;
 import com.chaotu.pay.po.TUserRates;
 import com.chaotu.pay.utils.MyMapper;
+import com.chaotu.pay.vo.UserRatesVo;
 import com.chaotu.pay.vo.UserVo;
 import org.apache.ibatis.annotations.Param;
 
@@ -10,7 +11,17 @@ import java.util.List;
 
 public interface TUserRatesMapper extends MyMapper<TUserRates> {
 
-    List<TUserRates> getUserRatesByUser(UserVo userVo);
+    /***
+     * 根据用户查询UserRates
+     * @param userVo
+     * @return
+     */
+    List<UserRatesVo> getUserRatesByUser(UserVo userVo);
 
+    /***
+     * 根据用户id,支付方式id修改userRates
+     * @param users
+     * @param userRates
+     */
     void updateByUserId(@Param("users")List<TUser> users,@Param("userRates")TUserRates userRates);
 }
